14b3e61072e1493c5d41c4def105a844e45a97ce
98woo <phantom02101@gmail.com>
2024-02-02 17:55:32 +0900

    2/2 마지막 저장

--- vending_machine/src/constants/AppendType.java
+++ vending_machine/src/constants/AppendType.java
@@ -1,5 +1,5 @@
 package constants;
 
 public enum AppendType {
-
+	APPEND, OVER_WRITE
 }
--- vending_machine/src/ref11_vending_machine_fileio/util/NIOFileUtil.java
+++ vending_machine/src/ref11_vending_machine_fileio/util/NIOFileUtil.java
@@ -1,5 +1,73 @@
 package ref11_vending_machine_fileio.util;
 
-public class NIOFileUtil {
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
+import constants.AppendType;
+
+public class NIOFileUtil {
+	
+	public static File findFile(String fileName, File from) {
+		if (from == null) {
+			from = new File("C:\\");
+		}
+		if (from.exists() && from.isDirectory()) {
+			File[] itemIndir = from.listFiles();
+			
+			if (itemIndir != null) {
+				for (File item : itemIndir) {
+					if (item.isDirectory()) {
+						File result = findFile(fileName, item);
+						if (result != null) {
+							return result;
+						}
+					}
+					else if (item.getName().equals(fileName)) {
+						return item;
+					}
+				}
+			}
+		}
+		else if (from.getName().equals(fileName)) {
+			return from;
+		}
+		return null;
+	}
+	
+	public static void writeFile(String parent, String name, String description, AppendType appendType) {
+		File file = new File(parent, name);
+		if(!file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();
+		}
+		
+		if ( appendType == AppendType.OVER_WRITE ) {
+			int index = 2;
+			while (file.exists()) {
+				file = new File("C:\\java\\outputs", "java output (" + (index++) + ").txt");
+			}
+		}
+		
+		List<String> fileDesc = new ArrayList<>();
+		fileDesc.add(description);
+		
+		try {
+//			Set<StrandardOpenOption> defualtOption =
+//			Set.of(StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+			if ( appendType == AppendType.APPEND ) {
+				Files.write(file.toPath(), fileDesc, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+			}
+			else {
+				Files.write(file.toPath(), fileDesc, Charset.forName("UTF-8"));
+			}
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println(file.getAbsolutePath());
+	}
 }
